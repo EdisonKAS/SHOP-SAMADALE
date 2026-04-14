3b8f096616dc2b37372a63be973ab2c4fe1ad856
EdisonKAS <edisonkamabu24@gmail.com>
2026-04-15 00:45:06 +0100

    Create messages.ts file with application messages constants for SHOP-SAMADALE project

---
+++ src/constants/messages.ts
@@ -0,0 +1,5 @@
+export const MESSAGES = {
+    WELCOME: 'Welcome to the SHOP-SAMADALE application!',
+    ERROR: 'An error has occurred. Please try again later.',
+    LOADING: 'Loading, please wait...'
+};
